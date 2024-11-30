6a505c4008d331486a209a034bcb0a234ba0c09e
ganka-xn <jxn@inbox.ru>
2024-11-30 12:35:05 +0300

    Task 5. ProfileRestController tests added. ProfileTestData refactoring.

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -2,11 +2,151 @@ package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
 
+import com.javarush.jira.common.util.JsonUtil;
+import com.javarush.jira.profile.ProfileTo;
+import com.javarush.jira.profile.internal.ProfileMapper;
+import com.javarush.jira.profile.internal.ProfileRepository;
+import com.javarush.jira.profile.internal.model.Profile;
 
-//todo
-// mockMVC + mockito + junit
-// Написать тесты для всех публичных методов контроллера ProfileRestController.
-// Хоть методов только 2, но тестовых методов должно быть больше, т.к. нужно проверить success and unsuccess path.
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.javarush.jira.login.internal.web.UserTestData.ADMIN_MAIL;
+import static com.javarush.jira.login.internal.web.UserTestData.USER_MAIL;
+import static com.javarush.jira.profile.internal.web.ProfileRestController.REST_URL;
+import static com.javarush.jira.profile.internal.web.ProfileTestData.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 class ProfileRestControllerTest extends AbstractControllerTest {
 
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Autowired
+    private ProfileMapper profileMapper;
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void shouldSucceeded_WhenUserAuthorized() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_DTO_MATCHER.contentJson(USER_PROFILE_DTO));
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void shouldSucceeded_WhenAdminAuthorized() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_DTO_MATCHER.contentJson(ADMIN_PROFILE_DTO));
+    }
+
+    @Test
+    void shouldFail_WhenUnauthorized() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @Transactional
+    @WithUserDetails(value = USER_MAIL)
+    void shouldSucceeded_WhenUpdateWithUserProfile() throws Exception {
+        Profile userProfileToUpdate = profileRepository.getExisted(USER_ID);
+        ProfileTo userProfileDTOtoUpdate = profileMapper.toTo(userProfileToUpdate);
+
+        userProfileDTOtoUpdate.setMailNotifications(USER_NOTIFICATIONS_UPDATED);
+        userProfileDTOtoUpdate.setContacts(USER_CONTACTS_UPDATED);
+
+        perform(
+                MockMvcRequestBuilders.put(REST_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtil.writeValue(userProfileDTOtoUpdate)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        Profile userDbProfileAfterUpdate = profileRepository.getExisted(USER_ID);
+        ProfileTo userDbProfileDTOAfterUpdate = profileMapper.toTo(userDbProfileAfterUpdate);
+
+        assertEquals(userProfileDTOtoUpdate, userDbProfileDTOAfterUpdate);
+    }
+
+    // alternative
+    @Test
+    @Transactional
+    @WithUserDetails(value = ADMIN_MAIL)
+    void shouldSucceeded_WhenUpdateWithAdminProfile() throws Exception {
+        ProfileTo adminProfileDTOtoUpdate = ProfileTestData.getUpdatedAdminDTO();
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(adminProfileDTOtoUpdate)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_DTO_MATCHER.contentJson(adminProfileDTOtoUpdate));
+    }
+
+    @Test
+    @Transactional
+    @WithUserDetails(value = ADMIN_MAIL)
+    void shouldFail_WhenUpdateWithInvalidDTO() throws Exception {
+        ProfileTo invalidDTO = getInvalidDTO();
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(invalidDTO)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @Transactional
+    @WithUserDetails(value = ADMIN_MAIL)
+    void shouldFail_WhenUpdateWithUnknownNotificationDTO() throws Exception {
+        ProfileTo unknownNotificationTo = getWithUnknownNotificationDTO();
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(unknownNotificationTo)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @Transactional
+    @WithUserDetails(value = ADMIN_MAIL)
+    void shouldFail_WhenUpdateWithUnknownContactDTO() throws Exception {
+        ProfileTo unknownContactTo = getWithUnknownContactDTO();
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(unknownContactTo)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    @Transactional
+    void shouldFail_WhenUpdateWithContactHtmlUnsafeDTO() throws Exception {
+        ProfileTo contactHtmlUnsafeTo = getWithContactHtmlUnsafeDTO();
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(contactHtmlUnsafeTo)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
 }
--- src/test/java/com/javarush/jira/profile/internal/web/ProfileTestData.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileTestData.java
@@ -3,84 +3,156 @@ package com.javarush.jira.profile.internal.web;
 import com.javarush.jira.MatcherFactory;
 import com.javarush.jira.profile.ContactTo;
 import com.javarush.jira.profile.ProfileTo;
-import com.javarush.jira.profile.internal.model.Contact;
-import com.javarush.jira.profile.internal.model.Profile;
 
 import java.util.Collections;
 import java.util.Set;
 
 public class ProfileTestData {
-    public static MatcherFactory.Matcher<Profile> PROFILE_MATCHER =
-            MatcherFactory.usingIgnoringFieldsComparator(Profile.class, "user");
 
-    public static ProfileTo USER_PROFILE_TO = new ProfileTo(null,
-            Set.of("assigned", "overdue", "deadline"),
-            Set.of(new ContactTo("skype", "userSkype"),
-                    new ContactTo("mobile", "+01234567890"),
-                    new ContactTo("website", "user.com")));
-    public static ProfileTo GUEST_PROFILE_EMPTY_TO = new ProfileTo(null,
-            Set.of(),
-            Set.of());
+    public static MatcherFactory.Matcher<ProfileTo> PROFILE_DTO_MATCHER =
+            MatcherFactory.usingIgnoringFieldsComparator(ProfileTo.class, "user");
 
-    public static ProfileTo getNewTo() {
-        return new ProfileTo(null,
-                Set.of("three_days_before_deadline", "two_days_before_deadline", "one_day_before_deadline"),
-                Set.of(new ContactTo("tg", "guestTg")));
+    public static final Long ADMIN_ID = 2L;
+    public static final Long USER_ID = 1L;
+
+    private static final String ASSIGNED = "assigned";
+    private static final String THREE_DAYS_BEFORE_DEADLINE = "three_days_before_deadline";
+    private static final String TWO_DAYS_BEFORE_DEADLINE = "two_days_before_deadline";
+    private static final String ONE_DAY_BEFORE_DEADLINE = "one_day_before_deadline";
+    private static final String DEADLINE = "deadline";
+    private static final String OVERDUE = "overdue";
+
+    private static final ContactTo USER_CONTACT_SKYPE = new ContactTo("skype", "userSkype");
+    private static final ContactTo USER_CONTACT_MOBILE = new ContactTo("mobile", "+01234567890");
+    private static final ContactTo USER_CONTACT_WEBSITE = new ContactTo("website", "user.com");
+
+    private static final ContactTo USER_CONTACT_SKYPE_UPDATED = new ContactTo("skype", "newSkype");
+    private static final ContactTo USER_CONTACT_MOBILE_UPDATED = new ContactTo("mobile", "+380987654321");
+    private static final ContactTo USER_CONTACT_WEBSITE_UPDATED = new ContactTo("website", "new.com");
+    private static final ContactTo USER_CONTACT_GITHUB_UPDATED = new ContactTo("github", "newGitHub");
+    private static final ContactTo USER_CONTACT_TG_UPDATED = new ContactTo("tg", "newTg");
+    private static final ContactTo USER_CONTACT_VK_UPDATED = new ContactTo("vk", "newVk");
+    private static final ContactTo USER_CONTACT_LINKEDIN_UPDATED = new ContactTo("linkedin", "newLinkedin");
+
+    private static final ContactTo ADMIN_CONTACT_GITHUB = new ContactTo("github", "adminGitHub");
+    private static final ContactTo ADMIN_CONTACT_TG = new ContactTo("tg", "adminTg");
+    private static final ContactTo ADMIN_CONTACT_VK = new ContactTo("vk", "adminVk");
+
+    private static final ContactTo ADMIN_CONTACT_GITHUB_UPDATED = new ContactTo("github", "newAdminGitHub");
+    private static final ContactTo ADMIN_CONTACT_TG_UPDATED = new ContactTo("tg", "newAdminTg");
+    private static final ContactTo ADMIN_CONTACT_VK_UPDATED = new ContactTo("vk", "newAdminVk");
+    private static final ContactTo ADMIN_CONTACT_SKYPE_UPDATED = new ContactTo("skype", "newAdminSkype");
+    private static final ContactTo ADMIN_CONTACT_MOBILE_UPDATED = new ContactTo("mobile", "newAdminMobile");
+    private static final ContactTo ADMIN_CONTACT_WEBSITE_UPDATED = new ContactTo("website", "newAdminWebsite");
+    private static final ContactTo ADMIN_CONTACT_LINKEDIN_UPDATED = new ContactTo("linkedin", "newAdminLinkedin");
+
+    public static final Set<String> USER_NOTIFICATIONS = Set.of(
+            ASSIGNED,
+            OVERDUE,
+            DEADLINE
+    );
+
+    public static final Set<ContactTo> USER_CONTACTS = Set.of(
+            USER_CONTACT_SKYPE,
+            USER_CONTACT_MOBILE,
+            USER_CONTACT_WEBSITE
+    );
+
+    public static final Set<String> USER_NOTIFICATIONS_UPDATED = Set.of(
+            ASSIGNED,
+            THREE_DAYS_BEFORE_DEADLINE,
+            TWO_DAYS_BEFORE_DEADLINE,
+            ONE_DAY_BEFORE_DEADLINE,
+            DEADLINE,
+            OVERDUE
+    );
+
+    public static final Set<ContactTo> USER_CONTACTS_UPDATED = Set.of(
+            USER_CONTACT_SKYPE_UPDATED,
+            USER_CONTACT_MOBILE_UPDATED,
+            USER_CONTACT_WEBSITE_UPDATED,
+            USER_CONTACT_GITHUB_UPDATED,
+            USER_CONTACT_TG_UPDATED,
+            USER_CONTACT_VK_UPDATED,
+            USER_CONTACT_LINKEDIN_UPDATED
+    );
+
+    public static final Set<String> ADMIN_NOTIFICATIONS = Set.of(
+            TWO_DAYS_BEFORE_DEADLINE,
+            ONE_DAY_BEFORE_DEADLINE,
+            THREE_DAYS_BEFORE_DEADLINE
+    );
+
+    public static final Set<ContactTo> ADMIN_CONTACTS = Set.of(
+            ADMIN_CONTACT_GITHUB,
+            ADMIN_CONTACT_TG,
+            ADMIN_CONTACT_VK
+    );
+
+    public static final Set<String> ADMIN_NOTIFICATIONS_UPDATED = Set.of(
+            THREE_DAYS_BEFORE_DEADLINE,
+            TWO_DAYS_BEFORE_DEADLINE,
+            ONE_DAY_BEFORE_DEADLINE,
+            DEADLINE
+    );
+
+    public static final Set<ContactTo> ADMIN_CONTACTS_UPDATED = Set.of(
+            ADMIN_CONTACT_GITHUB_UPDATED,
+            ADMIN_CONTACT_TG_UPDATED,
+            ADMIN_CONTACT_VK_UPDATED,
+            ADMIN_CONTACT_SKYPE_UPDATED,
+            ADMIN_CONTACT_MOBILE_UPDATED,
+            ADMIN_CONTACT_WEBSITE_UPDATED,
+            ADMIN_CONTACT_LINKEDIN_UPDATED
+    );
+
+    public static ProfileTo ADMIN_PROFILE_DTO = new ProfileTo(
+            ADMIN_ID,
+            ADMIN_NOTIFICATIONS,
+            ADMIN_CONTACTS
+    );
+
+    public static ProfileTo USER_PROFILE_DTO = new ProfileTo(
+            USER_ID,
+            USER_NOTIFICATIONS,
+            USER_CONTACTS
+    );
+
+    public static ProfileTo getUpdatedUserDTO() {
+        return new ProfileTo(
+                USER_ID,
+                USER_NOTIFICATIONS_UPDATED,
+                USER_CONTACTS_UPDATED
+        );
     }
 
-    public static Profile getNew(long id) {
-        Profile profile = new Profile();
-        profile.setId(id);
-        profile.setMailNotifications(14);
-        profile.setContacts(Set.of(new Contact(id, "tg", "guestTg")));
-        return profile;
+    public static ProfileTo getUpdatedAdminDTO() {
+        return new ProfileTo(
+                ADMIN_ID,
+                ADMIN_NOTIFICATIONS_UPDATED,
+                ADMIN_CONTACTS_UPDATED
+        );
     }
 
-    public static ProfileTo getUpdatedTo() {
-        return new ProfileTo(null,
-                Set.of("assigned", "three_days_before_deadline", "two_days_before_deadline", "one_day_before_deadline", "deadline", "overdue"),
-                Set.of(new ContactTo("skype", "newSkype"),
-                        new ContactTo("mobile", "+380987654321"),
-                        new ContactTo("website", "new.com"),
-                        new ContactTo("github", "newGitHub"),
-                        new ContactTo("tg", "newTg"),
-                        new ContactTo("vk", "newVk"),
-                        new ContactTo("linkedin", "newLinkedin")));
-    }
-
-    public static Profile getUpdated(long id) {
-        Profile profile = new Profile();
-        profile.setId(id);
-        profile.setMailNotifications(63);
-        profile.setContacts(Set.of(new Contact(id, "skype", "newSkype"),
-                new Contact(id, "mobile", "+380987654321"),
-                new Contact(id, "website", "new.com"),
-                new Contact(id, "github", "newGitHub"),
-                new Contact(id, "tg", "newTg"),
-                new Contact(id, "vk", "newVk"),
-                new Contact(id, "linkedin", "newLinkedin")));
-        return profile;
-    }
-
-    public static ProfileTo getInvalidTo() {
+    public static ProfileTo getInvalidDTO() {
         return new ProfileTo(null,
                 Set.of(""),
                 Set.of(new ContactTo("skype", "")));
     }
 
-    public static ProfileTo getWithUnknownNotificationTo() {
+    public static ProfileTo getWithUnknownNotificationDTO() {
         return new ProfileTo(null,
                 Set.of("WrongNotification"),
                 Collections.emptySet());
     }
 
-    public static ProfileTo getWithUnknownContactTo() {
+    public static ProfileTo getWithUnknownContactDTO() {
         return new ProfileTo(null,
                 Collections.emptySet(),
                 Set.of(new ContactTo("WrongContactCode", "contact")));
     }
 
-    public static ProfileTo getWithContactHtmlUnsafeTo() {
+    public static ProfileTo getWithContactHtmlUnsafeDTO() {
         return new ProfileTo(null,
                 Collections.emptySet(),
                 Set.of(new ContactTo("tg", "<script>alert(123)</script>")));
