95f79afcc8b3ac74e77aee8250178fa85894b809
ganka-xn <jxn@inbox.ru>
2024-12-09 20:19:29 +0300

    Task 4. Testcontainers

--- src/test/java/com/javarush/jira/BaseTests.java
+++ src/test/java/com/javarush/jira/BaseTests.java
@@ -1,9 +1,40 @@
 package com.javarush.jira;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @ActiveProfiles("test")
 abstract class BaseTests {
+
+    @Container
+    static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres")
+            .withPassword("inmemory")
+            .withUsername("inmemory");
+
+    static {
+        try {
+            postgreSQLContainer.start();
+        } catch (Exception e) {
+            throw new IllegalStateException("Cannot start PostgreSQL container", e);
+        }
+        postgreSQLContainer.waitingFor(Wait.forListeningPort());
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+    }
+
 }
